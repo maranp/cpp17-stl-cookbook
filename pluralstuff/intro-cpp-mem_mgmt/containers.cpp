f55bfecd4d6cdf50ce0b020f0e8d367cab925379
Maran Pakkirisamy <maran.pakkirisamy@cavium.com>
2018-04-04 13:02:09 +0530

    watch object destruction taken care of when an exception is thrown

--- pluralstuff/intro-cpp-mem_mgmt/containers.cpp
+++ pluralstuff/intro-cpp-mem_mgmt/containers.cpp
@@ -16,21 +16,26 @@ using std::endl;
 using std::vector;
 
 int main() {
-  Resource r("local");
-  cout << "----------------------------------------" << endl;
-  vector<Resource> v;
-  // reserve avoids moving (relocating) the vector for
-  // each push_back until the reserved number of entries are pushed back
-  //v.reserve(3);
-  v.push_back(Resource("first"));
-  // a construction, copy construction, a destruction
-  // are reduced to one construction (in the vector directly)
-  //v.emplace_back("first");
-  cout << "----------------------------------------" << endl;
-  v.push_back(Resource("second"));
-  cout << "----------------------------------------" << endl;
-  v.push_back(Resource("third"));
-  cout << "----------------------------------------" << endl;
+  try {
+    Resource r("local");
+    cout << "----------------------------------------" << endl;
+    vector<Resource> v;
+    // reserve avoids moving (relocating) the vector for
+    // each push_back until the reserved number of entries are pushed back
+    //v.reserve(3);
+    v.push_back(Resource("first"));
+    // a construction, copy construction, a destruction
+    // are reduced to one construction (in the vector directly)
+    //v.emplace_back("first");
+    cout << "----------------------------------------" << endl;
+    v.push_back(Resource("second"));
+    throw (std::bad_exception());
+    cout << "----------------------------------------" << endl;
+    v.push_back(Resource("third"));
+    cout << "----------------------------------------" << endl;
+  } catch (std::exception& e) {
+    cout << "exception caught" << endl;
+  }
   return 0;
 }
 
