90e4f24623960c580d6b6a85789bd2128cf091c4
Maran Pakkirisamy <maran.pakkirisamy@cavium.com>
2018-04-04 12:54:41 +0530

    demostrate object lifetime when adding entries to vector

--- pluralstuff/intro-cpp-mem_mgmt/Resource.h
+++ pluralstuff/intro-cpp-mem_mgmt/Resource.h
@@ -7,24 +7,25 @@ private:
   std::string name;
 public:
   Resource(std::string _name) : name { _name } {
-    std::cout << "constructing " << name << std::endl;
+    std::cout << "constructing " << name << " of " << this << std::endl;
   }
   Resource(Resource const & res) : name {res.name} {
-    std::cout << "copy constructing " << name << std::endl;
+    std::cout << "copy constructing " << name << " of " << this <<  std::endl;
   }
   Resource & operator=(Resource const & res) {
     // clean up "this" object's resources before assinment
     // before cleaning up make sure that this object and
     // to be assigned object (self assignment)  are not the same
-    std::cout << "copy assigning " << name << std::endl;
+    std::cout << "copy assigning " << name << " of " << this <<  std::endl;
     name = res.name;
     return *this;
   }
+  ~Resource() {
+    std::cout << "destructing " << name << " of " << this << std::endl;
+  }
+
   void SetResourceName(std::string res_name) {
     name = res_name;
-  }
-  ~Resource() {
-    std::cout << "destructing " << name << std::endl;
   }
   std::string GetName() const {
     return name;
---
+++ pluralstuff/intro-cpp-mem_mgmt/containers.cpp
@@ -0,0 +1,36 @@
+/*
+ * containers.cpp
+ *
+ *  Created on: 04-Apr-2018
+ *      Author: maran
+ */
+
+
+#include "Resource.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <vector>
+using std::vector;
+
+int main() {
+  Resource r("local");
+  cout << "----------------------------------------" << endl;
+  vector<Resource> v;
+  // reserve avoids moving (relocating) the vector for
+  // each push_back until the reserved number of entries are pushed back
+  //v.reserve(3);
+  v.push_back(Resource("first"));
+  // a construction, copy construction, a destruction
+  // are reduced to one construction (in the vector directly)
+  //v.emplace_back("first");
+  cout << "----------------------------------------" << endl;
+  v.push_back(Resource("second"));
+  cout << "----------------------------------------" << endl;
+  v.push_back(Resource("third"));
+  cout << "----------------------------------------" << endl;
+  return 0;
+}
+
